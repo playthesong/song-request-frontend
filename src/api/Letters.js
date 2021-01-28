ed7ed0966e0215102ecf895e05a7a9f0aa83c6e1
ragdoll <museopkim0214@gmail.com>
2021-01-28 22:24:47 +0900

    [#2] Feat: API 요청 분리
    api/Letters
    - 현재는 Mock 데이터를 받아오지만 추후 axios를 통한 REST 통신을 관리

---
+++ src/api/Letters.js
@@ -0,0 +1,13 @@
+import letters from "./mockData";
+
+const sleep = n => new Promise(resolve, setTimeout(resolve, n));
+
+export const getLetters = async () => {
+  await sleep(500);
+  return letters;
+};
+
+export const getLetterById = async id => {
+  await sleep(500);
+  return letters.find(letter => letter.id === id);
+};
