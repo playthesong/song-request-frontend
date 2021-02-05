691373a73f60a817eff7ff1e1658e08342ede48b
ragdoll <museopkim0214@gmail.com>
2021-02-06 03:43:05 +0900

    [#8] Fix: song 리듀서 async await Syntax 에러 수정

--- src/modules/song.js
+++ src/modules/song.js
@@ -4,12 +4,12 @@ const SEARCH_SONG = "song/SEARCH_SONG";
 const SEARCH_SONG_SUCCESS = "song/SEARCH_SONG_SUCCESS";
 const SEARCH_SONG_ERROR = "song/SEARCH_SONG_ERROR";
 
-const searchSong = (artist, title) => dispatch => {
+export const searchSong = (artist, title) => async dispatch => {
   dispatch({ type: SEARCH_SONG });
 
   try {
     const song = await songAPI.searchSong(artist, title);
-    dispatch({ type: SEARCH_SONG_SUCCESS, song});
+    dispatch({ type: SEARCH_SONG_SUCCESS, song });
   } catch (error) {
     dispatch({ type: SEARCH_SONG_ERROR });
   }
