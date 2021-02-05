ab74608fe4f1a35d73608b3e7c519eb30cafb742
ragdoll <museopkim0214@gmail.com>
2021-02-06 02:48:15 +0900

    [#8] Feat: Song API, 스토어 관리 로직 추가
    mock 데이터를 연동 하도록 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import letters from "./letters";
 import letter from "./letter";
 import letterModal from "./letterModal";
+import song from "./song";
 
 const rootReducer = combineReducers({
   letters,
   letter,
-  letterModal
+  letterModal,
+  song
 });
 
 export default rootReducer;
---
+++ src/modules/song.js
@@ -0,0 +1,49 @@
+import * as songAPI from "../api/song";
+
+const SEARCH_SONG = "song/SEARCH_SONG";
+const SEARCH_SONG_SUCCESS = "song/SEARCH_SONG_SUCCESS";
+const SEARCH_SONG_ERROR = "song/SEARCH_SONG_ERROR";
+
+const searchSong = (artist, title) => dispatch => {
+  dispatch({ type: SEARCH_SONG });
+
+  try {
+    const song = await songAPI.searchSong(artist, title);
+    dispatch({ type: SEARCH_SONG_SUCCESS, song});
+  } catch (error) {
+    dispatch({ type: SEARCH_SONG_ERROR });
+  }
+};
+
+const initialState = {
+  data: null,
+  loading: false,
+  error: null
+};
+
+function song(state = initialState, action) {
+  switch (action.type) {
+    case SEARCH_SONG:
+      return {
+        data: null,
+        loading: true,
+        error: null
+      };
+    case SEARCH_SONG_SUCCESS:
+      return {
+        data: action.song,
+        loading: false,
+        error: null
+      };
+    case SEARCH_SONG_ERROR:
+      return {
+        data: null,
+        loading: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
+
+export default song;
