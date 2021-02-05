fbd88cea682953887bf02b107d3461a2028475ca
ragdoll <museopkim0214@gmail.com>
2021-02-06 03:41:01 +0900

    [#8] Feat: SongSearchResultContainer 추가
    스토어에서 Song 데이터를 받아오는 Container 컴포넌트 추가

--- src/containers/SongSearchResultContainer.jsx
+++ src/containers/SongSearchResultContainer.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import SongSearchResult from "../components/Letter/LetterEditor/SongSearchModal/SongSearchResult";
+import { searchSong } from "../modules/song";
 
 const SongSearchResultContainer = () => {
-  return <LetterSearchModalResult />;
+  const { data: songs, loading, error } = useSelector(state => state.song);
+
+  if (error) {
+    return <div>ERROR!</div>;
+  }
+
+  if (!songs) {
+    return null;
+  }
+
+  return <SongSearchResult songs={songs} />;
 };
 
 export default SongSearchResultContainer;
