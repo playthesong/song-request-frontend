34af3f2e89e2079e13b0777278ed8aa6e90f101b
ragdoll <museopkim0214@gmail.com>
2021-02-06 01:27:56 +0900

    [#8] Feat: 신청곡 검색 결과 mock 데이터 추가

---
+++ src/containers/SongSearchResultContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SongSearchResultContainer = () => {
+  return <LetterSearchModalResult />;
+};
+
+export default SongSearchResultContainer;
---
+++ src/mockData/searchResult.js
@@ -0,0 +1,54 @@
+const searchResult = [
+  {
+    title: "감사",
+    artist: "김동률",
+    imageUrl: "http://i.maniadb.com/images/album/148/148138_1_f.jpg"
+  },
+  {
+    title: "끝나지않은노래",
+    artist: "노 리플라이",
+    imageUrl: "http://i.maniadb.com/images/album/407/407690_1_f.jpg"
+  },
+  {
+    title: "영원히",
+    artist: "성시경",
+    imageUrl: "http://i.maniadb.com/images/album/803/803789_1_f.jpg"
+  },
+  {
+    title: "내가 되었으면",
+    artist: "노 리플라이",
+    imageUrl: "http://i.maniadb.com/images/album/639/639756_1_f.jpg"
+  },
+  {
+    title: "밤편지",
+    artist: "아이유",
+    imageUrl: "http://i.maniadb.com/images/album/742/742576_1_f.jpg"
+  },
+  {
+    title: "수고했어, 오늘도",
+    artist: "옥상달빛",
+    imageUrl: "http://i.maniadb.com/images/album/661/661987_1_f.jpg"
+  },
+  {
+    title: "그대 내게 기대",
+    artist: "에피톤 프로젝트",
+    imageUrl: "http://i.maniadb.com/images/album/728/728107_1_f.jpg"
+  },
+  {
+    title: "두 사람",
+    artist: "성시경",
+    imageUrl: "http://i.maniadb.com/images/album/137/137851_1_f.jpg"
+  },
+  {
+    title: "민물장어의 꿈",
+    artist: "신해철",
+    imageUrl: "http://i.maniadb.com/images/album/105/105444_cda_f.jpg"
+  },
+  {
+    title: "사랑하기 때문에",
+    artist: "양희은",
+    imageUrl: "http://i.maniadb.com/images/album/132/132298_1_f.jpg"
+  }
+];
+
+export default searchResult;
