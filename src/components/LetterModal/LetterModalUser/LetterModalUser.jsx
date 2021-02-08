3aecafd6c7d13d3244089b808292ff7cf27b0f8b
ragdoll <museopkim0214@gmail.com>
2021-02-08 19:02:26 +0900

    [#8] Refactor: LetterModalUser 컴포넌트 일부 병합
    분리 할 필요가 없는 컴포넌트 병합

--- src/components/LetterModal/LetterModalUser/LetterModalUser.jsx
+++ src/components/LetterModal/LetterModalUser/LetterModalUser.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import CreatedDateTime from "./CreatedDateTime";
 import UserAbout from "./UserAbout";
 
 const LetterModalUser = ({ user, createdDateTime }) => {
   const { username, avatarUrl } = user;
   return (
     <LetterUserModalBlock>
-      <CreatedDateTime createdDateTime={createdDateTime} />
+      <CreatedDateTime>{createdDateTime}</CreatedDateTime>
       <UserAbout username={username} avatarUrl={avatarUrl} />
     </LetterUserModalBlock>
   );
@@ -18,6 +17,12 @@ const LetterUserModalBlock = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-top: 3.5rem;
+`;
+
+const CreatedDateTime = styled.span`
+  margin-top: 1.2rem;
+  font-size: 0.8rem;
+  opacity: 0.6;
 `;
 
 export default LetterModalUser;
