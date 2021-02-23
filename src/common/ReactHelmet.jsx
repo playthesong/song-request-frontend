460f19591e715d8f57d58580ae00ac780185205e
ragdoll <museopkim0214@gmail.com>
2021-02-24 00:30:51 +0900

    [#13] Fix: Mixed Contents 오류 발생하여 meta 태그 제거

--- src/common/ReactHelmet.jsx
+++ src/common/ReactHelmet.jsx
@@ -4,10 +4,6 @@ import { Helmet } from "react-helmet";
 const ReactHelmet = () => {
   return (
     <Helmet>
-      <meta
-        http-equiv="Content-Security-Policy"
-        content="upgrade-insecure-requests"
-      ></meta>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=swap"
