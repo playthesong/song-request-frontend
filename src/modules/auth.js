d3bf5bae8543c85bbe3604f3311410e08ecb0bcd
ragdoll <museopkim0214@gmail.com>
2021-03-05 19:20:07 +0900

    [#23] Feat: auth 리덕스 스토어 추가

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,15 +1,55 @@
 import * as authAPI from "../api/auth";
+import jwt from "jsonwebtoken";
 
 const LOGIN = "auth/LOGIN";
 const LOGIN_SUCCESS = "auth/LOGIN_SUCCESS";
-const LOGIN_FAILURE = "auth/LOGIN_FAILURE";
+const LOGIN_ERROR = "auth/LOGIN_FAILURE";
 
-export const login = async dispatch => {
+export const login = generationKey => async dispatch => {
   dispatch({ type: LOGIN });
 
   try {
-    const response = await authAPI.login();
+    const jwtToken = await authAPI.login(generationKey);
+    const { user } = jwt.decode(jwtToken, process.env.REACT_APP_JWT_SECRET_KEY);
+    dispatch({ type: LOGIN_SUCCESS, jwtToken, user });
   } catch (error) {
-    console.log(error);
+    dispatch({ type: LOGIN_ERROR, error });
   }
 };
+
+const initialState = {
+  user: null,
+  jwtToken: null,
+  loading: false,
+  error: null
+};
+
+function auth(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        user: null,
+        jwtToken: null,
+        loading: true,
+        error: null
+      };
+    case LOGIN_SUCCESS:
+      return {
+        user: action.user,
+        jwtToken: action.jwtToken,
+        loading: false,
+        error: null
+      };
+    case LOGIN_ERROR:
+      return {
+        user: null,
+        jwtToken: null,
+        loading: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
+
+export default auth;
