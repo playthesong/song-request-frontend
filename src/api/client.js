171b02baf355d2242ae2a7c558ab578afd427a2c
ragdoll <museopkim0214@gmail.com>
2021-04-01 19:35:33 +0900

    [#31] Feat: Auth 관련 API 이외에 JWT 토큰이 필요한 API에 대해 localStorage로부터 토큰을 헤더에 추가 하도록 axios 설정

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AUTH } from "../constants/auth";
 
 export const client = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
@@ -12,5 +13,11 @@ export const client = axios.create({
 
 export const authClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-  withCredentials: true
+  withCredentials: true,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Access-Control-Expose-Headers": "*",
+    Authorization: AUTH.BEARER + localStorage.getItem(AUTH.JWT_TOKEN)
+  }
 });
