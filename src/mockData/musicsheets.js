56c66679ccdaa5991dfa1eea1ad8407da6c66ee0
ragdoll <museopkim0214@gmail.com>
2021-04-05 00:14:44 +0900

    [#34] Feat: Product 페이지 구현

---
+++ src/mockData/musicsheets.js
@@ -0,0 +1,245 @@
+import realpiano01 from "../assets/realpiano_01.jpeg";
+import realpiano02 from "../assets/realpiano_02.jpeg";
+import realpiano03 from "../assets/realpiano_03.jpeg";
+import realpiano04 from "../assets/realpiano_04.jpeg";
+import realpiano05 from "../assets/realpiano_05.jpeg";
+import realpiano06 from "../assets/realpiano_06.jpeg";
+import realpiano07 from "../assets/realpiano_07.jpeg";
+import realpiano08 from "../assets/realpiano_08.jpeg";
+import realpiano09 from "../assets/realpiano_09.jpeg";
+import realpiano10 from "../assets/realpiano_10.jpeg";
+import realpiano11 from "../assets/realpiano_11.jpeg";
+import realpiano12 from "../assets/realpiano_12.jpeg";
+import realpiano13 from "../assets/realpiano_13.jpeg";
+import realpiano14 from "../assets/realpiano_14.jpeg";
+
+const Vol_1 = {
+  id: 1,
+  songs: [
+    "스윗소로우 - 아무리 생각해도 난 너를",
+    "김광진 - 편지",
+    "박정운 - 오늘같은 밤이면",
+    "김현철 - 일생을",
+    "아리랑",
+    "거미 - 그대 돌아오면",
+    "이적 - 다행이다",
+    "태연 - 들리나요",
+    "등대지기"
+  ],
+  imageUrl: realpiano01
+};
+const Vol_2 = {
+  id: 2,
+  songs: [
+    "오 신실하신 주",
+    "목마른 사슴",
+    "주님의 시간에",
+    "내안에 있는 그 이름",
+    "저 높은 곳을 향하여",
+    "예수 그 이름",
+    "주는 평화",
+    "하나님은 너를 지키시는 자",
+    "예수 나를 위하여"
+  ],
+  imageUrl: realpiano02
+};
+const Vol_3 = {
+  id: 3,
+  songs: [
+    "정엽 - Nothing better",
+    "박지윤 - 환상",
+    "캐롤라인 크루거 - You call it love",
+    "장혜진 - 1994년 어느 늦은 밤",
+    "변진섭 - 그대 내게 다시",
+    "성시경 - 내게 오는 길",
+    "현빈 - 그 남자",
+    "윤상 - 가려진 시간 사이로",
+    "최호섭 - 세월이 가면"
+  ],
+  imageUrl: realpiano03
+};
+const Vol_4 = {
+  id: 4,
+  songs: [
+    "전람회 - 기억의 습작",
+    "버스커버스커 - 여수밤바다",
+    "토이 - 내가 너의 곁에 잠시 살았다는걸",
+    "페퍼톤스 - 공원여행",
+    "패닉 - 달팽이",
+    "ABBA - Thank you for the music",
+    "유재하 - 가리워진 길",
+    "성시경 - 두사람",
+    "김건모 - 첫인상"
+  ],
+  imageUrl: realpiano04
+};
+const Vol_5 = {
+  id: 5,
+  songs: [
+    "버스커버스커 - 벚꽃엔딩",
+    "전람회 - 취중진담",
+    "조용필 - 이젠 그랬으면 좋겠네",
+    "성시경 - 넌 감동이었어",
+    "유재하 - 그대 내 품에",
+    "김건모 - 아름다운 이별",
+    "신승훈 - I Believe",
+    "서지원 - 내 눈물 모아",
+    "양희은 - 사랑 그 쓸쓸함에 대하여"
+  ],
+  imageUrl: realpiano05
+};
+const Vol_6 = {
+  id: 6,
+  songs: [
+    "거룩하신 하나님",
+    "주님께 감사드리라",
+    "내 맘에 한 노래 있어",
+    "오직 주만이",
+    "전능하신 나의 주 하나님은",
+    "주 여호와는 광대하시도다",
+    "나를 받으옵소서",
+    "주 말씀 향하여",
+    "주의 음성을 내가 들으니"
+  ],
+  imageUrl: realpiano06
+};
+const Vol_7 = {
+  id: 7,
+  songs: [
+    "박효신 - 눈의 꽃",
+    "토이 - 여전히 아름다운지",
+    "성시경 - 거리에서",
+    "이소라 - 제발",
+    "공일오비 - 어디선가 나의 노랠 듣고 있을 너에게",
+    "일기예보 - 인형의 꿈",
+    "김형중 - 그랬나봐",
+    "강수지 - 보라빛 향기",
+    "가을방학 - 가끔 미치도록 네가 안고 싶어질 때가 있어"
+  ],
+  imageUrl: realpiano07
+};
+const Vol_8 = {
+  id: 8,
+  songs: [
+    "김동률 - 감사",
+    "임창정 - 소주 한잔",
+    "유재하 - 사랑하기 때문에",
+    "박정현 - 편지할게요",
+    "태양 - 눈 코 입",
+    "더 클래식 - 마법의 성",
+    "팀 - 사랑합니다",
+    "제이레빗 - 요즘 너 말야",
+    "윤종신 - 애니",
+    "김현식 - 내 사랑 내 곁에"
+  ],
+  imageUrl: realpiano08
+};
+const Vol_9 = {
+  id: 9,
+  songs: [
+    "플라이 투더 스카이 - Missin You",
+    "박효신 - 동경",
+    "김동률 - 다시 사랑한다 말할까",
+    "공일오비 - 1월부터 6월까지",
+    "이은미 - 애인 있어요",
+    "이적 - 거짓말 거짓말 거짓말",
+    "토이 - 오늘 서울은 하루종일 맑음",
+    "박정현 - 나의 하루",
+    "이소라 - 난 행복해"
+  ],
+  imageUrl: realpiano09
+};
+const Vol_10 = {
+  id: 10,
+  songs: [
+    "그 사랑",
+    "달고 오묘한 그 말씀",
+    "세상의 유혹 시험이",
+    "예배합니다",
+    "내 영혼의 그윽히 깊은데서",
+    "주만 바라볼찌라",
+    "주의 거룩하심 생각할 때",
+    "주가 보이신 생명의 길",
+    "참 아름다워라"
+  ],
+  imageUrl: realpiano10
+};
+const Vol_11 = {
+  id: 11,
+  songs: [
+    "김동률 - Replay",
+    "성시경 - 너는 나의 봄이다",
+    "거미 - 친구라도 될 걸 그랬어",
+    "김윤아 - 봄날은 간다",
+    "소녀시대 - 다시 만난 세계",
+    "보보 - 늦은 후회",
+    "김동률 - 이제서야",
+    "김종국 - 한 남자",
+    "바비킴 - 사랑 그 놈"
+  ],
+  imageUrl: realpiano11
+};
+const Vol_12 = {
+  id: 12,
+  songs: [
+    "아이유 - 밤편지",
+    "윤종신 - 좋니",
+    "이하이 - 한숨",
+    "김동률 - 답장",
+    "성시경 - 한번 더 이별",
+    "한동근 - 그대라는 사치",
+    "싸이 - 어땠을까",
+    "애즈원 - 원하고 원망하죠",
+    "에피톤 프로젝트 - 나는 그 사람이 아프다"
+  ],
+  imageUrl: realpiano12
+};
+const Vol_13 = {
+  id: 13,
+  songs: [
+    "엠씨더맥스 - 어디에도",
+    "이승환 - 어떻게 사랑이 그래요",
+    "이적 - 걱정말아요 그대",
+    "폴 킴 - 모든 날 모든 순간",
+    "김동률 - 잔향",
+    "백지영 - 잊지 말아요",
+    "성시경 - 안녕 나의 사랑",
+    "유재하 - 내 마음에 비친 내 모습",
+    "이문세 - 옛사랑"
+  ],
+  imageUrl: realpiano13
+};
+const Vol_14 = {
+  id: 14,
+  songs: [
+    "이승환 - 천일동안",
+    "임재범 - 너를 위해",
+    "방탄소년단(BTS) - 봄날",
+    "이소라 - 내곁에서 떠나가지 말아요",
+    "뱅크 - 가질 수 없는 너",
+    "유재하 - 우울한 편지",
+    "이문세 - 사랑이 지나가면",
+    "왁스 - 부탁해요",
+    "양파 - 애송이의 사랑"
+  ],
+  imageUrl: realpiano14
+};
+
+const products = [
+  Vol_1,
+  Vol_2,
+  Vol_3,
+  Vol_4,
+  Vol_5,
+  Vol_6,
+  Vol_7,
+  Vol_8,
+  Vol_9,
+  Vol_10,
+  Vol_11,
+  Vol_12,
+  Vol_13,
+  Vol_14
+];
+
+export default products;
--- src/pages/MusicSheets.jsx
+++ src/pages/MusicSheets.jsx
@@ -1,16 +1,135 @@
-import React from "react";
-import EmptyResult from "../components/EmptyResult/EmptyResult";
+import React, { useState } from "react";
+import styled from "styled-components";
 import MainTemplate from "../components/Template/Main/MainTemplate";
+import useModal from "../hooks/useModal";
+import products from "../mockData/musicsheets";
 
 const MusicSheets = () => {
+  const [id, setId] = useState(null);
+  const [isOpened, openModal, closeModal] = useModal();
+
+  const inActivateScroll = () => {
+    document.body.style.overflow = "hidden";
+    document.body.scroll = "no";
+  };
+
+  const activateScroll = () => {
+    document.body.style.overflow = "scroll";
+    document.body.scroll = "yes";
+  };
+
+  const onOpenModal = id => {
+    setId(id);
+    openModal();
+    inActivateScroll();
+  };
+
+  const onCloseModal = () => {
+    setId(null);
+    closeModal();
+    activateScroll();
+  };
+
   return (
-    <MainTemplate>
-      <EmptyResult
-        message={"아직 준비 중입니다 :) 조금만 기다려주세요!"}
-        opacity={0.3}
-      />
-    </MainTemplate>
+    <>
+      <MainTemplate>
+        <MusicSheetsBlock>
+          {products.map(product => (
+            <MusicSheet
+              key={product.id}
+              src={product.imageUrl}
+              onClick={() => onOpenModal(product.id)}
+            />
+          ))}
+        </MusicSheetsBlock>
+      </MainTemplate>
+      <DimmedBackground isOpened={isOpened}>
+        <Title>수록곡</Title>
+        <TrackList>
+          {products
+            .filter(product => product.id === id)
+            .map(product => product.songs)
+            .map(songs =>
+              songs.map((song, index) => (
+                <Track key={index}>{`${index + 1}. ${song}`}</Track>
+              ))
+            )}
+        </TrackList>
+        <CloseButton onClick={onCloseModal}>닫기</CloseButton>
+      </DimmedBackground>
+    </>
   );
 };
+
+const MusicSheetsBlock = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 80%;
+`;
+
+const MusicSheet = styled.img`
+  max-width: 21rem;
+  max-height: 21rem;
+  margin: 5rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.2),
+    0 8px 16px -8px rgba(0, 0, 0, 0.2), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+  transition: 0.3s;
+
+  &:hover {
+    box-shadow: 0px 25px 30px 3px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const DimmedBackground = styled.div`
+  display: ${props => (props.isOpened ? "flex" : "none")};
+  position: fixed;
+  width: 100vw;
+  min-height: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  background: rgba(0, 0, 0, 0.95);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h3`
+  font-size: 1.7rem;
+  color: #fff;
+  padding: 1.1rem 0rem;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+`;
+
+const TrackList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 1.5rem;
+`;
+
+const Track = styled.li`
+  color: rgba(255, 255, 255, 0.8);
+  padding: 1.5rem;
+  font-size: 1.5rem;
+`;
+
+const CloseButton = styled.button`
+  margin-top: 3rem;
+  padding: 0.5rem 1.7rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 0.3rem;
+  background-color: #f06595;
+  color: #fff;
+  box-shadow: 5px 1px 35px 1px rgba(255, 255, 255, 0.25);
+`;
 
 export default MusicSheets;
