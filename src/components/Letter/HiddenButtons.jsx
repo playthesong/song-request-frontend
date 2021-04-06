9b2069857ca7e9ab9664150ef90c76c4492f4027
ragdoll <museopkim0214@gmail.com>
2021-04-06 22:23:39 +0900

    [#46] Feat: 신청 대기 상태로 변경하는 버튼 구현

--- src/components/Letter/HiddenButtons.jsx
+++ src/components/Letter/HiddenButtons.jsx
@@ -3,12 +3,16 @@ import styled, { css } from "styled-components";
 import { FiCheck } from "react-icons/fi";
 import { BiGitCompare } from "react-icons/bi";
 import { AiTwotoneDelete } from "react-icons/ai";
+import { IoTimeOutline } from "react-icons/io5";
 import { LETTER_STATUS } from "../../constants/letterStatus";
 
 const HiddenButtons = ({ openMenu, onOpenDeleteModal, onChangeStatus }) => {
   return (
     <>
       <HiddenButtonsBlock openMenu={openMenu}>
+        <WaitingIcon
+          onClick={event => onChangeStatus(event, LETTER_STATUS.WAITING)}
+        />
         <CheckIcon
           onClick={event => onChangeStatus(event, LETTER_STATUS.DONE)}
         />
@@ -26,7 +30,7 @@ const HiddenButtonsBlock = styled.div`
   position: relative;
   flex-direction: column;
   margin-top: 2.1rem;
-  margin-left: 0.25rem;
+  margin-left: 0.5rem;
   z-index: 9;
   ${props =>
     props.openMenu
@@ -39,16 +43,23 @@ const HiddenButtonsBlock = styled.div`
 `;
 
 const IconCommonStyles = css`
-  margin: 0.65rem 0rem;
-  margin-right: 1rem;
+  margin: 0.63rem 0rem;
+  margin-right: 0.5rem;
   border-radius: 50%;
   color: #fff;
   box-shadow: 7px 3px 16px 3px rgba(50, 50, 50, 0.2);
   transition: 0.2s;
 
   &:hover {
-    padding: 0.6rem 0.6rem;
+    padding: 0.65rem 0.65rem;
   }
+`;
+
+const WaitingIcon = styled(IoTimeOutline)`
+  padding: 0.35rem 0.45rem;
+  font-size: 1.6rem;
+  background-color: #74c0fc;
+  ${IconCommonStyles}
 `;
 
 const CheckIcon = styled(FiCheck)`
