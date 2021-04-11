847788941bd764018510d6a76e76b6d4e9191312
ragdoll <museopkim0214@gmail.com>
2021-04-12 00:07:04 +0900

    [#46] Fix: Privacy Policy 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const FooterBlock = styled.div`
   justify-content: center;
   box-sizing: border-box;
   width: 100%;
-  height: 11rem;
+  height: 12.5rem;
   margin-top: 6rem;
   background-color: #1e2429;
   font-weight: 500;
@@ -39,7 +39,8 @@ const FooterInner = styled.div`
 `;
 
 const PrivacyPolicy = styled(Link)`
-  margin-top: 1.5rem;
+  margin-top: 1.2rem;
+  margin-bottom: 1rem;
   font-size: 1rem;
 `;
 
